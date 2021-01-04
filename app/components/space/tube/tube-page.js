b6d9ea9e7d2305ca169e082b8fbe704e9c338f25
Joshua0707 <oladapoodelola@gmail.com>
2021-01-04 23:52:46 +0100

    feature - Add video basic functionlities

--- app/components/space/tube/tube-page.js
+++ app/components/space/tube/tube-page.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Slider from '@react-native-community/slider';
 import { StyleSheet, View, Text, TouchableOpacity, FlatList, ScrollView, TextInput, Image, ActivityIndicator } from 'react-native'
 import { connect } from 'react-redux'
 import * as MyUserActions from '../../../../redux/MyUser/actions'
@@ -9,9 +10,10 @@ import LinearGradient from 'react-native-linear-gradient'
 import CommentList from '../../core/comment-list'
 import { getStatusBarHeight } from 'react-native-iphone-x-helper'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faAngleDown, faPaperPlane } from '@fortawesome/pro-light-svg-icons'
+import { faAngleDown, faPaperPlane, faPause, faPlay, faThumbsUp } from '@fortawesome/pro-light-svg-icons'
 import * as TubePageActions from '../../../../redux/TubePage/actions'
 import { getDateTranslated } from '../../../services/translation/translation-service'
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 class TubePage extends React.Component {
 
@@ -19,12 +21,61 @@ class TubePage extends React.Component {
         super(props)
         this.state = {
             commentView: false,
-            videoReady: false
+            videoReady: false,
+            video: {
+                isPaused: false,
+                currentTime: 0,
+                duration: 0.1,
+                overlay: false
+            }
         }
     }
 
+    testValue = [
+        { id: "", tube: { posterLink: "https://i.pinimg.com/236x/c7/a1/b8/c7a1b863aeba4b9a409b61ad7201924b.jpg", profile: { _meta: { pseudo: "Best locations to visit in LONDON" }, pictureprofile: "" } } },
+        { id: "", tube: { posterLink: "https://unsplash.com/photos/UzZhBohuFXo", profile: { _meta: { pseudo: "VLOG - Our trip tp NEWYORK!" }, pictureprofile: "" } } },
+        { id: "", tube: { posterLink: "https://unsplash.com/photos/_6zo6Qo-iVo", profile: { _meta: { pseudo: "Most Commonly visited places" }, pictureprofile: "" } } },
+        { id: "", tube: { posterLink: "https://images.unsplash.com/photo-1512552288940-3a300922a275?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8dmFjYXRpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", profile: { _meta: { pseudo: "Beautify locations to spend your vacation" }, pictureprofile: "" } } },
+        { id: "", tube: { posterLink: "https://images.unsplash.com/photo-1528277342758-f1d7613953a2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fGFmcmljYXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", profile: { _meta: { pseudo: "AFRICA the world wonder" }, pictureprofile: "" } } }
+    ]
+
     UNSAFE_componentWillMount = () => {
         this.uploadPageTube(this.props.screenProps.rootNavigation.state.params.tubeId)
+    }
+
+    // video actions
+    playPauseVideo = () => {
+        if (this.state.videoReady) {
+            this.setState((prevState) => {
+                return ({
+                    video: { ...prevState.video, isPaused: !prevState.video.isPaused }
+                })
+            });
+        }
+        clearTimeout(this.overlayTimer)
+        this.setState({ video: { ...this.state.video, overlay: true } })
+        this.overlayTimer = setTimeout(() => this.setState({ video: { ...this.state.video, overlay: false } }), 3000);
+    }
+    onVideoLoad = ({ duration }) => this.setState({ video: { ...this.state.video, duration } });
+    onVideoProgress = ({ currentTime }) => this.setState({ video: { ...this.state.video, currentTime } });
+    onVideoEnd = () => {
+        this.setState({ video: { ...this.state.video, isPaused: false }});
+        this.video.seek(0);
+    }
+    onSliderSeek = (slide) => {
+        this.player.seek(slide * this.state.video.duration);
+    }
+
+    getTime = t => {
+        const digit = n => n < 10 ? `0${n}` : `${n}`;
+        // const t = Math.round(time);
+        const sec = digit(Math.floor(t % 60));
+        const min = digit(Math.floor((t / 60) % 60));
+        const hr = digit(Math.floor((t / 3600) % 60));
+        return hr === digit(0) ? min + ':' + sec : hr + ':' + min + ':' + sec; // this will convert sec to timer string
+        // 33 -> 00:00:33
+        // this is done here
+        // ok now the theme is good to look
     }
 
     // to upload the page of the tube
@@ -35,51 +86,95 @@ class TubePage extends React.Component {
 
     // to display the header view
     _headerRender = () => {
+        const { duration, currentTime, overlay, isPaused } = this.state.video;
         return (
             <View>
+                {/* Video section */}
+                <View style={styles.videoSection}>
+                    {/* Video */}
+                    <Video
+                        ref={(ref) => this.player = ref}
+                        onReadyForDisplay={() => this.setState({ videoReady: true })}
+                        style={{ width: '100%', height: '100%', backgroundColor: 'black' }}
+                        source={{ uri: this.props.TubePage.tube.videoLink }}
+                        repeat={true}
+                        minLoadRetryCount={5}
+                        volume={0.1}
+                        resizeMode={'cover'}
+                        controls={false}
+                        paused={isPaused}
+                        onLoad={this.onVideoLoad}
+                        onProgress={this.onVideoProgress}
+                        onEnd={this.onVideoEnd}
+                    />
+                    {/* Poster */}
+                    <TouchableOpacity
+                        onPress={this.playPauseVideo}
+                        style={{ ...styles.overlay, backgroundColor: 'black' }}>
+                        {
+                            this.state.videoReady ? ( // controls if ready
+                                <>
+                                    { overlay && ( // check for overlay
+                                        <View style={styles.overlay}>
+                                            <LinearGradient
+                                                start={{ x: 0, y: 0 }}
+                                                end={{ x: 0, y: 1 }}
+                                                colors={['transparent', '#000000a0']}
+                                                style={{ ...styles.overlay, justifyContent: 'center', alignItems: 'center' }}
+                                            >
+                                                {/* <TouchableOpacity onPress={this.playPauseVideo} style={{ width: 50, height: 50, borderRadius: 30, justifyContent: 'center', alignItems: 'center' }}>
+                                                    <FontAwesomeIcon icon={this.state.video.isPaused ? faPlay : faPause} color={"#FFFFFF"} size={25} />
+                                                </TouchableOpacity> */}
 
-                {/* Video */}
-                <Video
-                    controls={true}
-                    onReadyForDisplay={() => this.setState({ videoReady: true })}
-                    style={{ width: '100%', height: 250, backgroundColor: 'black' }}
-                    source={{ uri: this.props.TubePage.tube.videoLink }}
-                    repeat={true}
-                    minLoadRetryCount={5}
-                    volume={0.1}
-                    resizeMode={'cover'}
-                />
+                                                <View style={{ position: 'absolute', left: 0, bottom: 0, right: 0, flexDirection: 'row', paddingVertical: 15, paddingHorizontal: 10 }}>
+                                                    <Text style={{ color: 'white', maxWidth: 40 }}>{ this.getTime(this.state.video.currentTime) }</Text>
+                                                    <View style={{ flex: 1, marginHorizontal: 10 }}>
+                                                    <Slider
+                                                        minimumTrackTintColor="#307ecc"
+                                                        maximumTrackTintColor="#000000"
+                                                        value={currentTime / duration}
+                                                        onValueChange={this.onSliderSeek} />
+                                                    </View>
+                                                    <Text style={{ color: 'white', maxWidth: 40 }}>{ this.getTime(this.state.video.duration) }</Text>
+                                                </View>
 
-                {/* Poster */}
-                {this.state.videoReady ? null :
-                    <View style={{ width: '100%', height: 250, position: 'absolute', top: 0 }}>
-                        <FastImage
-                            style={{ flex: 1 }}
-                            source={{ uri: this.props.TubePage.tube.posterLink, priority: FastImage.priority.normal }}
-                            resizeMode={FastImage.resizeMode.cover}
-                        />
-                        <View style={{ backgroundColor: '#00000045', position: 'absolute', width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
-                            <ActivityIndicator size='large' color="#ffffff" />
-                        </View>
-                    </View>
-                }
-
+                                            </LinearGradient>
+                                        </View>
+                                    ) }
+                                </>
+                            ) : ( // poster if not ready
+                                <View style={{ width: '100%', height: 250, position: 'absolute', top: 0 }}>
+                                    <FastImage
+                                        style={{ flex: 1 }}
+                                        source={{ uri: this.props.TubePage.tube.posterLink, priority: FastImage.priority.normal }}
+                                        resizeMode={FastImage.resizeMode.cover}
+                                    />
+                                    <View style={{ backgroundColor: '#00000045', position: 'absolute', width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+                                        <ActivityIndicator size='large' color="#ffffff" />
+                                    </View>
+                                </View>
+                            )
+                        }
+                    </TouchableOpacity>
+                </View>
 
                 {/* Profile */}
-                <View style={{ height: 70, flexDirection: 'row', alignItems: 'center', paddingHorizontal: 15, borderBottomColor: '#cdcdcd', borderBottomWidth: 0.5 }}>
-                    <View style={{ flex: 3, alignItems: 'center' }}>
-                        <FastImage
-                            style={{ width: 45, height: 45, borderRadius: 45, borderWidth: 2, borderColor: '#df0ddf' }}
-                            resizeMode={FastImage.resizeMode.cover}
-                            source={{ uri: this.props.TubePage.tube.profile.pictureprofile, priority: FastImage.priority.normal }}
-                        />
+                <View style={{ height: 70, flexDirection: 'row', alignItems: 'center', paddingHorizontal: 15 }}>
+                    <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
+                        <View style={{ marginEnd: 15 }}>
+                            <FastImage
+                                style={{ width: 45, height: 45, borderRadius: 45, borderWidth: 2, borderColor: '#df0ddf' }}
+                                resizeMode={FastImage.resizeMode.cover}
+                                source={{ uri: this.props.TubePage.tube.profile.pictureprofile, priority: FastImage.priority.normal }}
+                            />
+                        </View>
+                        <View style={{ }}>
+                            <Text style={{ fontWeight: 'bold', fontFamily: 'Avenir-Heavy' }}>{this.props.TubePage.tube.name}</Text>
+                            <Text>{getDateTranslated(this.props.TubePage.tube.createdAt)} </Text>
+                        </View>
                     </View>
-                    <View style={{ flex: 7 }}>
-                        <Text>{this.props.TubePage.tube.name}</Text>
-                        <Text>{getDateTranslated(this.props.TubePage.tube.createdAt)} </Text>
-                    </View>
-                    <View style={{ flex: 3, alignItems: 'center' }}>
-                        <LinearGradient
+                    <View style={{ }}>
+                        {/* <LinearGradient
                             start={{ x: 0, y: 1 }}
                             end={{ x: 1, y: 0 }}
                             colors={['#3C349B', '#8E46DF']}
@@ -88,10 +183,21 @@ class TubePage extends React.Component {
                             <TouchableOpacity>
                                 <Text style={{ textAlign: 'center', paddingVertical: 12, paddingHorizontal: 15, fontWeight: '700', color: 'white' }}>Follow</Text>
                             </TouchableOpacity>
-                        </LinearGradient>
+                        </LinearGradient> */}
+                        <FontAwesomeIcon icon={faThumbsUp} size={20} color="#77838F" />
+                        <Text style={{ color: "#77838F", marginTop: 5 }}>{ this.props.TubePage.tube.totalLike }</Text>
                     </View>
                 </View>
+            </View>
+        )
+    }
 
+    // to display the title and description
+    _titleRender = () => {
+        return (
+            <View style={{ paddingHorizontal: 15, paddingTop: 15 }}>
+                <Text style={{ fontSize: 22, fontWeight: 'bold' }}>{ 'Best places in NEW YORK!' }</Text>
+                <Text style={{ fontSize: 14, marginTop: 5 }}>{ 'Discover the most incredible places ...' }</Text>
             </View>
         )
     }
@@ -136,9 +242,11 @@ class TubePage extends React.Component {
     _suggestionTubeRender = () => {
         return (
             <ScrollView style={{ height: '100%' }}>
+                {/* Title */}
+                {this._titleRender()}
                 {this._tubeListBySection(this.props.TubePage.tubesFollower, 'CORE.From-X', { value: this.props.TubePage.tube.profile._meta.pseudo }, true)}
                 {this._tubeListBySection(this.props.TubePage.tubesSuggestions, 'CORE.Suggestion', false)}
-                {this.state.commentView ? null : this._footerComment()}
+                {/* {this.state.commentView ? null : this._footerComment()} */}
             </ScrollView>
         )
     }
@@ -149,7 +257,7 @@ class TubePage extends React.Component {
     }
 
     // to display some tube by section
-    _tubeListBySection = (tubeList, title, line) => {
+    _tubeListBySection = (tubeList=this.testValue, title, line) => {
 
         if (!tubeList || tubeList.length == 0) return null
 
@@ -178,7 +286,6 @@ class TubePage extends React.Component {
 
     // to display a miniature tube
     _oneTubeRender = (item) => {
-
         return (
             <TouchableOpacity
                 onPress={() => this.uploadPageTube(item.tube._id)}
@@ -187,11 +294,14 @@ class TubePage extends React.Component {
 
                 {/* Background Image */}
                 <FastImage
-                    style={{ width: '100%', height: '100%', borderRadius: 8 }} resizeMode={FastImage.resizeMode.cover}
+                    style={styles.oneTubeImage} resizeMode={FastImage.resizeMode.cover}
                     source={{ uri: item.tube.posterLink, priority: FastImage.priority.normal }}
                 />
+                <View style={{ paddingVertical: 10 }}>
+                    <Text style={styles.greyText}>{item.tube.profile._meta.pseudo}</Text>
+                </View>
 
-                {/* Footer Card */}
+                {/* Footer Card
                 <View style={{ position: 'absolute', bottom: 0, width: '100%', height: 70, }}>
                     <LinearGradient
                         colors={['#fbfbfb00', '#bdc3c72e', '#2c3e50d1']}
@@ -205,7 +315,8 @@ class TubePage extends React.Component {
                         </View>
                     </LinearGradient>
 
-                </View>
+                </View> */}
+
             </TouchableOpacity>
         )
 
@@ -222,7 +333,7 @@ class TubePage extends React.Component {
                         showsHorizontalScrollIndicator={false}
                         style={{ flexDirection: 'row', flexWrap: 'wrap', marginTop: 7, paddingLeft: 19 }}
                         data={tubeList}
-                        keyExtractor={(item) => item.id.toString()}
+                        keyExtractor={(item, index) => `${item.id.toString()}-${index}`}
                         renderItem={({ item }) => this._oneTubeRender(item)}
                     />
                 </View>
@@ -261,14 +372,36 @@ const styles = StyleSheet.create({
     oneTubeContainer: {
         marginHorizontal: 10,
         marginVertical: 15,
-        height: 155,
-        width: 175,
-        borderRadius: 8,
+        width: 160
+    },
+    oneTubeImage: {
+        borderRadius: 14,
+        height: 110,
+        width: '100%',
+        backgroundColor: 'white',
         shadowColor: "#000",
-        shadowOffset: { width: 0, height: 2 },
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
-        elevation: 5
+        elevation: 2,
+    },
+    greyText: {
+        color: '#77838F',
+        fontSize: 14,
+        fontFamily: 'Avenir-Heavy'
+    },
+    videoSection: {
+        height: 250,
+        width: '100%', 
+        height: 250, 
+        backgroundColor: 'black',
+        position: 'relative'
+    },
+    overlay: {
+        ...StyleSheet.absoluteFillObject
     }
 })
 
